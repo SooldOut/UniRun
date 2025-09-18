85a2aee4a9b75de3f03f9a15d59721c0784c6206
SooldOut <abracadabra9834@gmail.com>
2025-09-18 16:29:50 +0900

    20250918
    발판 만들기, 배경만들기, 장애물(559p까지 진행완료)

--- Assets/01_Scripts/PlayerController.cs
+++ Assets/01_Scripts/PlayerController.cs
@@ -71,7 +71,10 @@ public class PlayerController : MonoBehaviour
         playerRigidbody.linearVelocity = Vector2.zero;
         // 사망 상태를 true로 변경
         isDead = true;
+        // 게임 매니저의 게임오버 처리 실행
+        GameManager.instance.OnPlayerDead();
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
--- Assets/01_Scripts/ScrollingObject.cs
+++ Assets/01_Scripts/ScrollingObject.cs
@@ -8,6 +8,10 @@ public class ScrollingObject : MonoBehaviour
     private void Update()
     {
         // 초당 speed의 속도로 왼쪽으로 평행이동
-        transform.Translate(Vector3.left *  speed * Time.deltaTime);
+        if (!GameManager.instance.isGameover)
+        {
+            // 초당 speed의 속도로 왼쪽으로 평행이동
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
